bbb534db638cdbdb45213bb30feca911df9585d0
weixiao619 <l243224118@163.com>
2019-04-15 11:19:56 +0800

    python 实现的tcp服务

---
+++ tcp_py/tcpClit.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from socket import *
+
+HOST = '119.3.33.100'
+PORT = 7758
+BUFSIZ = 1024
+ADDR = (HOST, PORT)
+
+tcpCliSock = socket(AF_INET, SOCK_STREAM)
+tcpCliSock.connect(ADDR)
+
+while True:
+    data = input('> ')
+    if not data:
+        break
+    tcpCliSock.send(bytes(data, 'utf-8'))
+    data = tcpCliSock.recv(BUFSIZ)
+    if not data:
+        break
+    print(data.decode('utf-8'))
+
+tcpCliSock.close()
